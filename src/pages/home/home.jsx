cae965a4c45e1d3cdc5b831c6c0e9da9f4d095e3
erujs <a.m.jerus@gmail.com>
2024-03-04 21:19:20 +0800

    homepage update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,7 @@ import { AnimalContext } from '../../contexts/animalContext';
 import { Link } from 'react-router-dom';
 import reactLogo from '../../assets/react.svg';
 import { Eru } from '../../components/eru/eru';
+import { TechStack } from '../../components/techstack/techstack';
 import { Dog, Cat } from '../../components/svg/svg';
 
 const HomeView = () => {
@@ -13,28 +14,25 @@ const HomeView = () => {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center min-h-screen text-center px-16 py-16">
+    <div className="flex flex-col justify-center items-center min-h-screen px-16 py-16">
       <Eru />
+      <TechStack />
       <div className="container text-white">
-        <h1 className="text-5xl font-bold py-5">Welcome to Canes Feles!</h1>
-        <p className="text-lg">A dogs and cats browser made with</p>
+        <h1 className="text-4xl text-center font-bold py-5">Welcome to Canes Feles!</h1>
+        <div className='flex flex-col items-center'>
+          <p className='pb-2'>Discover adorable dogs and cats, each with delightful descriptions.</p>
+          <p className='pb-2'>Experience blazing speed – no ruff days here! Faster than a cat chasing a laser pointer!</p>
+          <p className='pb-2'>Enjoy sleek vibes with no hairballs – modern design as clean as a groomed furball.</p>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="container">
-        <p className="text-lg text-white">Select your preference:</p>
-        <div className="flex flex-col md:flex-row justify-center pt-8">
+      <div className="container py-4">
+        <p className="text-white text-center pb-4">Choose an image to start:</p>
+        <div className="flex flex-col md:flex-row justify-center">
           <Link to={'/cat'}>
-            <Cat className={'w-[16rem] lg:w-full'} />
+            <Cat className={'w-[16rem]'} />
           </Link>
           <Link to={'/dog'}>
-            <Dog className={'w-[16rem] lg:w-full'} />
+            <Dog className={'w-[16rem]'} />
           </Link>
         </div>
       </div>
